b6a72b8a2a4f073491af76b91ab30394830f325d
ReaprMan <ryan.logan01@gmail.com>
2017-06-17 19:32:52 -0500

    use of # with hex

--- 02_numeral-systems/03_hex/main.go
+++ 02_numeral-systems/03_hex/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Printf("%d - %b - %x", 20, 20, 20)
+	fmt.Printf("%d - %b - %x", 20, 20, 20)
 	//fmt.Printf("%d - %b - %#x", 20, 20, 20)
 	//fmt.Printf("%d - %b - %#X", 20, 20, 20)
-	fmt.Printf("%d \t %b \t %#x", 20, 20, 20)
+	//fmt.Printf("%d \t %b \t %#x", 20, 20, 20)
 
 }
