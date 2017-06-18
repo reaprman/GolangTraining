73a0ebaf212fb9274b03dc3f1d4c5875f0ac4f16
ReaprMan <ryan.logan01@gmail.com>
2017-06-17 19:38:46 -0500

    adding loop example

--- 02_numeral-systems/03_hex/main.go
+++ 02_numeral-systems/03_hex/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Printf("%d - %b - %x", 20, 20, 20)
 	//fmt.Printf("%d - %b - %#x", 20, 20, 20)
+	// # adds Ox
 	//fmt.Printf("%d - %b - %#X", 20, 20, 20)
 	//fmt.Printf("%d \t %b \t %#x", 20, 20, 20)
 
