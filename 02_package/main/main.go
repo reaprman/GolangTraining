9a4b1bc40157ccff18ee1c7bb7dc6c88283d9bfb
ReaprMan <ryan.logan01@gmail.com>
2017-06-17 23:20:51 -0500

    folder name correction: 02_package

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/reaprman/GolangTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.myName)
+}
